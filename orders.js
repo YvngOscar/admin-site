baada26af7c0763b3a4fa7e9169a660c1f98dc00
Yvngscar <oscarmwas65@gmail.com>
2023-02-18 13:56:38 +0300

    .js

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumer: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Larvender KF102 Drone",
+    productNumer: "36378",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumer: "49347",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    productNumer: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumer: "80000",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4k  Drone",
+    productNumer: "22821",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
